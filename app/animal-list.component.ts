ba28cbfde903e42fe5e0303cf33eff879ce303f4
Patrick Napper <patrick@patricknapper.com>
2017-12-08 10:24:04 -0800

    add animal-list component and animal.model.ts.

---
+++ app/animal-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+
+    <h2 *ngFor="let currentAnimal of childAnimalList" (click)="showDetails(currentAnimal)"><strong>{{currentAnimal.species}}:</strong> <strong>{{currentAnimal.name}}</strong>
+    </h2>
+
+    <div *ngIf="currentAnimal">
+    <hr>
+      <ul>
+        <li (click)="hideDetails(currentAnimal)">
+        <h3><strong>Species: {{currentAnimal.species}}</strong></h3>
+        <h3><strong>Name: {{currentAnimal.name}}</strong></div><br>  <div>Age: {{currentAnimal.age}}</div><br>
+        <div>Diet: {{currentAnimal.diet}}</div><br>
+        <div>Location: {{currentAnimal.location}}</div><br>  <div>Caretakers: {{currentAnimal.caretakers}}</div><br>
+        <div>Sex: {{currentAnimal.sex}}</div><br>
+        <div>Likes: {{currentAnimal.likes}}</div><br>  <div>Dislikes: {{currentAnimal.dislikes}}</div><br>
+        </li><br><br>
+      </ul>
+    </div>
+  `
+})
+
+export class AnimalListComponent {
+
+  @Input() childAnimalList: Animal[];
+  @Output() clickSender = new EventEmitter();
+
+  currentAnimal = null;
+
+  showDetails(clickedAnimal) {
+  this.currentAnimal = clickedAnimal;
+  }
+
+  hideDetails(clickedAnimal) {
+  this.currentAnimal = null;
+  }
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -6,26 +7,24 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Local Zoo - Animal List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-    <ul>
-       <li *ngFor="let currentAnimal of animals">{{currentAnimal.species}} {{currentAnimal.name}} {{currentAnimal.age}} {{currentAnimal.diet}} {{currentAnimal.location}} {{currentAnimal.caretakers}} {{currentAnimal.sex}} {{currentAnimal.likes}} {{currentAnimal.dislikes}}</li>
-    </ul>
+    <animal-list [childAnimalList]="masterAnimalList"></animal-list>
   </div>
   `
 })
 
 export class AppComponent {
   currentFocus: string = 'Animals';
+  selectedAnimal = null;
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  animals: Animal[] = [
+
+  masterAnimalList: Animal[] = [
     new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+
     new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+
     new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 5, 'Female', 'Delicate roots and leaves', 'Loud noises')
   ];
-}
-
-export class Animal {
-  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { }
 }
